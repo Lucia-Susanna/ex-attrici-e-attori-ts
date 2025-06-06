6356660506f6bd917130c1c0c72b3f2937dbdf7e
Lucia-Susanna <lucia_susanna@hotmail.it>
2025-06-06 13:33:03 +0200

    feat: milestone 3

--- src/main.ts
+++ src/main.ts
@@ -26,3 +26,49 @@ type Actress = Person & {
   awards: string;
   nationality: ActressNationality;
 }
+
+function isActress(dati: unknown): dati is Actress {
+  return (
+    typeof dati === 'object' &&
+    dati !== null &&
+    "id" in dati &&
+    typeof dati.id === 'number' &&
+    "name" in dati &&
+    typeof dati.name === 'string' &&
+    "birth_year" in dati &&
+    typeof dati.birth_year === 'number' &&
+    "death_year" in dati &&
+    typeof dati.death_year === 'number' &&
+    "biography" in dati &&
+    typeof dati.biography === 'string' &&
+    "image" in dati &&
+    typeof dati.image === 'string' &&
+    "most_famous_movies" in dati &&
+    dati.most_famous_movies instanceof Array &&
+    dati.most_famous_movies.length === 3 &&
+    dati.most_famous_movies.every(movie => typeof movie === 'string') &&
+    "awards" in dati &&
+    typeof dati.awards === 'string' &&
+    "nationality" in dati &&
+    typeof dati.nationality === 'string'
+
+  )
+}
+
+async function getActress(id: number): Promise<Actress | null> {
+  try {
+    const response = await fetch(`http://localhost:3333/actresses/${id}`)
+    const dati: unknown = await response.json()
+    if (!isActress(dati)) {
+      throw new Error('formato dati non valido')
+    }
+    return dati
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error('errore durente il recupero dell attrice:', error)
+    } else {
+      console.error('Errore sconosciuto:', error)
+    }
+    return null
+  }
+}
